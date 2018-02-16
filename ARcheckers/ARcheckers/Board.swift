78581dfb93f7426d802491d481bfee7c08e9d12c
Denis Malykh <mrdekk@yandex-team.ru>
2018-02-16 21:07:05 +0500

    plane detection

---
+++ ARcheckers/ARcheckers/Board.swift
@@ -0,0 +1,64 @@
+//
+//  Board.swift
+//  Checkers
+//
+//  Created by Denis Malykh on 28.01.18.
+//  Copyright © 2018 MrDekk. All rights reserved.
+//
+
+import Foundation
+import ARKit
+
+class Board : SCNNode {
+
+    private(set) var anchor: ARPlaneAnchor
+    private(set) var planeGeometry: SCNPlane? = nil
+
+    init(anchor: ARPlaneAnchor) {
+        self.anchor = anchor
+        super.init()
+
+        let geom = SCNPlane(
+            width: CGFloat(anchor.extent.x),
+            height: CGFloat(anchor.extent.z)
+        )
+        self.planeGeometry = geom
+
+        let mtrl = SCNMaterial()
+        //let img = UIImage(named: "tron_grid")
+        //mtrl.diffuse.contents = img
+        let clr = UIColor.cyan.withAlphaComponent(0.25)
+        mtrl.diffuse.contents = clr
+        geom.materials = [mtrl]
+
+        let node = SCNNode(geometry: geom)
+        node.position = SCNVector3(anchor.center.x, 0, anchor.center.z)
+        node.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1.0, 0.0, 0.0)
+
+        updateTexture()
+        addChildNode(node)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    func update(_ anchor: ARPlaneAnchor) {
+        planeGeometry?.width = CGFloat(anchor.extent.x)
+        planeGeometry?.height = CGFloat(anchor.extent.z)
+
+        position = SCNVector3(anchor.center.x, 0.0, anchor.center.z)
+        updateTexture()
+    }
+
+    private func updateTexture() {
+        let w = planeGeometry?.width ?? 0
+        let h = planeGeometry?.height ?? 0
+
+        if let mtrl = planeGeometry?.materials.first {
+            mtrl.diffuse.contentsTransform = SCNMatrix4MakeScale(Float(w), Float(h), 1)
+            mtrl.diffuse.wrapS = .repeat
+            mtrl.diffuse.wrapT = .repeat
+        }
+    }
+}
--- ARcheckers/ARcheckers/ViewController.swift
+++ ARcheckers/ARcheckers/ViewController.swift
@@ -1,25 +1,62 @@
 //
 //  ViewController.swift
-//  ARcheckers
+//  Checkers
 //
-//  Created by user on 16.02.18.
-//  Copyright © 2018 Yandex. All rights reserved.
+//  Created by Denis Malykh on 28.01.18.
+//  Copyright © 2018 MrDekk. All rights reserved.
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var sceneView: ARSCNView! {
+        didSet {
+            sceneView.delegate = self
+        }
+    }
+
+    private var boards: [UUID: Board] = [:]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let conf = ARWorldTrackingConfiguration()
+        conf.planeDetection = .horizontal
+        sceneView.session.run(conf)
+
+        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
     }
 
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
 }
 
+extension ViewController : ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let a = anchor as? ARPlaneAnchor {
+            let b = Board(anchor: a)
+            boards[anchor.identifier] = b
+            node.addChildNode(b)
+        }
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let b = boards[anchor.identifier], let a = anchor as? ARPlaneAnchor {
+            b.update(a)
+        }
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        boards[anchor.identifier] = nil
+    }
+}
