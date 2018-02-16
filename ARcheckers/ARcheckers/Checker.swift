c05f784c1a49b576328d9f40e9cb6ae90e39a6f8
Nikolay <gladkovskiy.ns@gmail.com>
2018-02-16 22:12:42 +0500

    Checker

---
+++ ARcheckers/ARcheckers/Checker.swift
@@ -0,0 +1,36 @@
+//
+//  Checker.swift
+//  ARcheckers
+//
+//  Created by user on 16.02.18.
+//  Copyright © 2018 Yandex. All rights reserved.
+//
+
+import ARKit
+
+class Checker: SCNNode {
+
+    public let isWhite = true
+    
+    
+    override init() {
+        super.init()
+        let geom = SCNCylinder(radius: 0.02, height: 0.01)
+        
+        let mtrl = SCNMaterial()
+        
+        let clr = isWhite ? UIColor.white : UIColor.black
+        mtrl.diffuse.contents = clr
+        geom.materials = [mtrl]
+        
+        
+        
+        
+        addChildNode(node)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+}
